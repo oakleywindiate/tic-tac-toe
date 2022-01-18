49c81297b7b5886157d839728fae1cba489c7442
Oakley Windiate <oakley.windiate@gmail.com>
2022-01-18 13:42:27 -0700

    Update game board constructor

--- game.js
+++ game.js
@@ -1,16 +1,8 @@
 class Game {
   constructor(player1, player2) {
-    this.firstPlayer = player1.firstPlayer;
-    this.secondPlayer = player2.secondPlayer;
-    this.firstTurn = player1.firstTurn;
-    this.secondTurn = player2.secondTurn;
+    this.players = [player1, player2];
+    this.turn = 1;
     this.points = 0;
-    this.wins = 0;
-  }
-  draw() {
-
-  }
-  reset() {
-
+    this.gameBoard = new Array(9).fill(null);
   }
 }
