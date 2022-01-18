53df0f0ef407070222700a931b95da351640d688
Oakley Windiate <oakley.windiate@gmail.com>
2022-01-18 09:13:50 -0700

    Created emojis for board and CSS

--- game.js
+++ game.js
@@ -1,8 +1,11 @@
 class Game {
-  constructor(player) {
-    this.turn = player.turn;
-    this.points = player.points;
-    this.wins = player.wins;
+  constructor(player1, player2) {
+    this.firstPlayer = player1.firstPlayer;
+    this.secondPlayer = player2.secondPlayer;
+    this.firstTurn = player1.firstTurn;
+    this.secondTurn = player2.secondTurn;
+    this.points = 0;
+    this.wins = 0;
   }
   draw() {
 
