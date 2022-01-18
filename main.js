974ff5ec31c791fa73b4a0396012167868aaffb1
Oakley Windiate <oakley.windiate@gmail.com>
2022-01-18 13:36:43 -0700

    Create event listener for allGameSquares

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 
+/* -------- QUERY SELECTORS --------*/
+
+
+
+/* -------- VARIABLES --------*/
+
+var allGameSquares = document.getElementById('allGameSquares');
+
+/* -------- EVENT LISTENERS --------*/
+
+
+/* -------- VARIABLES --------*/
+
+
+/* -------- FUNCTIONS --------*/
