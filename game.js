a3a1cd2a6f20959e9dce8316ed0967b5657a98cd
Oakley Windiate <oakley.windiate@gmail.com>
2022-01-18 21:57:57 -0700

    Add completed function for checking wins

--- game.js
+++ game.js
@@ -13,4 +13,40 @@ class Game {
     }
       this.gameBoard[index] = player.emoji;
   }
+  win() {
+    if((game.gameBoard[0] !== null && game.gameBoard[0] === game.gameBoard[1]
+      && game.gameBoard[1] === game.gameBoard[2])) {
+        return true;
+      }
+    if((game.gameBoard[3] !== null && game.gameBoard[3] === game.gameBoard[4]
+      && game.gameBoard[4] === game.gameBoard[5])) {
+        return true;
+      }
+    if((game.gameBoard[6] !== null && game.gameBoard[6] === game.gameBoard[7]
+        && game.gameBoard[7] === game.gameBoard[8])) {
+          return true;
+      }
+    if((game.gameBoard[0] !== null && game.gameBoard[0] === game.gameBoard[3]
+      && game.gameBoard[3] === game.gameBoard[6])) {
+        return true;
+      }
+    if((game.gameBoard[1] !== null && game.gameBoard[1] === game.gameBoard[4]
+      && game.gameBoard[4] === game.gameBoard[7])) {
+        return true;
+      }
+    if((game.gameBoard[2] !== null && game.gameBoard[2] === game.gameBoard[5]
+      && game.gameBoard[5] === game.gameBoard[8])) {
+        return true;
+      }
+    if((game.gameBoard[0] !== null && game.gameBoard[0] === game.gameBoard[4]
+      && game.gameBoard[4] === game.gameBoard[8])) {
+        return true;
+      }
+    if((game.gameBoard[2] !== null && game.gameBoard[2] === game.gameBoard[4]
+      && game.gameBoard[4] === game.gameBoard[6])) {
+        return true;
+      } else {
+        return false
+      }
+  }
 }
--- main.js
+++ main.js
@@ -7,14 +7,28 @@ var player2 = new Player(2, 'spaceship');
 var game = new Game(player1, player2);
 
 
+var combinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 /* -------- QUERY SELECTORS --------*/
 
 var allGameSquares = document.getElementById('allGameSquares');
+var winnerBanner = document.querySelector('.outcome-banner');
+var square = document.getElementById('square');
 
 /* -------- EVENT LISTENERS --------*/
 
 allGameSquares.addEventListener('click', function(e) {
   setEmoji(e);
+  determineWin(e);
 });
 
 /* -------- FUNCTIONS --------*/
@@ -26,5 +40,15 @@ function setEmoji(e) {
   if (game.gameBoard[boardIndex] === null) {
     game.gameplay(boardIndex, currentPlayer)
     e.target.classList += ` ${currentPlayer.emoji}`;
+  }
+}
+
+function determineWin() {
+  if (game.win() === true && game.players[0] === player1) {
+    winnerBanner.innerHTML += `PLAYER 1 WINS!`
+  } else {
+  if(game.win() === true && game.players[1] === player2) {
+      winnerBanner.innerHTML += `PLAYER 2 WINS!`
+    }
   }
 }
